b868b1f90189fe4ee7d564e759efa98a1a65d2c8
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:20:50 +0100

    Better naming files

--- include/ft_printfchars.h
+++ include/ft_printfchars.h
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_ints.h                                   :+:      :+:    :+:   */
+/*   ft_printfchars.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/15 11:32:03 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/15 14:47:13 by pnguyen-         ###   ########.fr       */
+/*   Created: 2023/11/15 11:39:46 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/15 14:49:00 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_INTS_H
-# define FT_PRINTF_INTS_H
+#ifndef FT_PRINTF_CHARS_H
+# define FT_PRINTF_CHARS_H
 
-#include <stdarg.h>
 #include <sys/types.h>
+#include <stdarg.h>
 
 #include "ft_printf.h"
 #include "ft_printfparser.h"
-#include "ft_printf_utils.h"
 
-ssize_t	ft_printfhexnull(t_flags flags, t_uint width);
-ssize_t	ft_printfptr(va_list ap, t_flags flags, t_uint width);
-ssize_t	ft_printfintd(va_list ap, t_flags flags, t_uint width, ssize_t precision);
+ssize_t	ft_printfchar(va_list ap, t_flags, t_uint width);
+ssize_t	ft_printfstr(va_list ap, t_flags, t_uint width, ssize_t precision);
+
 #endif
--- include/ft_printfints.h
+++ include/ft_printfints.h
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_chars.h                                  :+:      :+:    :+:   */
+/*   ft_printfints.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/15 11:39:46 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/15 14:49:00 by pnguyen-         ###   ########.fr       */
+/*   Created: 2023/11/15 11:32:03 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/15 14:47:13 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_CHARS_H
-# define FT_PRINTF_CHARS_H
+#ifndef FT_PRINTF_INTS_H
+# define FT_PRINTF_INTS_H
 
-#include <sys/types.h>
 #include <stdarg.h>
+#include <sys/types.h>
 
 #include "ft_printf.h"
 #include "ft_printfparser.h"
+#include "ft_printf_utils.h"
 
-ssize_t	ft_printfchar(va_list ap, t_flags, t_uint width);
-ssize_t	ft_printfstr(va_list ap, t_flags, t_uint width, ssize_t precision);
-
+ssize_t	ft_printfhexnull(t_flags flags, t_uint width);
+ssize_t	ft_printfptr(va_list ap, t_flags flags, t_uint width);
+ssize_t	ft_printfintd(va_list ap, t_flags flags, t_uint width, ssize_t precision);
 #endif
