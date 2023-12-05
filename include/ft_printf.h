9bc85423d02692bf76bc44e580c9f1263a848f0c
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:20:11 +0100

    Renamed the typedef so it is normed

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:00:17 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/15 11:46:13 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/15 14:49:31 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-typedef unsigned int uint;
+typedef unsigned int t_uint;
 
 int		ft_printf(char const format[], ...);
 
