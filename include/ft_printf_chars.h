04c9f95bfe4243e39458b32bb6fa63734772633a
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:20:50 +0100

    name of the typedef corrected

--- include/ft_printf_chars.h
+++ include/ft_printf_chars.h
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:39:46 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/15 11:40:43 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/15 14:49:00 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include "ft_printf.h"
 #include "ft_printfparser.h"
 
-ssize_t	ft_printfchar(va_list ap, t_flags, uint width);
-ssize_t	ft_printfstr(va_list ap, t_flags, uint width, ssize_t precision);
+ssize_t	ft_printfchar(va_list ap, t_flags, t_uint width);
+ssize_t	ft_printfstr(va_list ap, t_flags, t_uint width, ssize_t precision);
 
 #endif
