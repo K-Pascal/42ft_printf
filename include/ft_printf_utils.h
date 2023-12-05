6d08a7cde75d8b3a70b72750fd34190712b2d676
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:20:50 +0100

    Updated functions prototype

--- include/ft_printf_utils.h
+++ include/ft_printf_utils.h
@@ -6,16 +6,19 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 11:43:33 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/15 11:56:43 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/15 17:38:27 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_UTILS_H
 # define FT_PRINTF_UTILS_H
-typedef unsigned int t_uint;
+
+#include "ft_printf.h"
 
 void	padding_char(unsigned int len, char c);
 void	ft_puthex(unsigned long nbr, int uppercase);
-int		get_numdigits(unsigned long nbr, int base);
+int		get_numudigits(unsigned long nbr, int base);
+int		get_numdigits(int nbr, int base);
+void	ft_putnbr(int nbr, int *index, int end);
 
 #endif
