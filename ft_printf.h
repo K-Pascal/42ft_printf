12ce767b8e09491eecad632274e081099c5045cc
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-21 14:43:23 +0100

    precision is now an unsigned int instead of a ssize_t

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:00:17 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/12/05 20:22:50 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/09 16:22:39 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ typedef unsigned int	t_uint;
 typedef struct s_nums
 {
 	t_uint	width;
-	ssize_t	precision;
+	t_uint	precision;
 }	t_nums;
 
 int		ft_printf(char const format[], ...);
